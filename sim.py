a774aac77e7a50a74563fc3cf8df4da5d8114abe
Some_Dumb_Dude <154841872+Tuxedp@users.noreply.github.com>
2026-02-07 16:51:00 -0500

    Pseudocode

--- sim.py
+++ sim.py
@@ -1,14 +1,24 @@
-# Dependencies (ase, pymatgen)
+# Dependencies (ase)
 from ase.io import read, write
 from ase.visualize import view
 
 # Add a MOF into the simulation through reading the .cif file
 
-# Code to find the center of the MOF's pores in order to add the TFSi anion
+# Find the center of the MOF's pores in order to add the TFSi anion
+# Use 2 atoms as a reference for center coordinates and then center inside based on another elements position
+
+# Add the TFSi anion inside the MOF pores (2 anions)
+# Rotate accoridngly
 
 # Place Li cations near the Mg sites in the MOF
+# Li should be lined up near the Nitrogen on the anion for proper bonding
+# Base position based on the Mg site and Nitrogen
 
 # Sourround the MOF with  solvent (CH3OCH3)
+# Define a region that solvent sould not appear
+# Randomly generate a certain amount of solvent around the simulation box
+# If the molecule is touching the MOF, generate a new positon
+# Loop until all molecules of solvent are added
 
 # Perform an energy minimization to relax the solution
-# Output the relaxed system to an xyz file
+# Output the relaxed system to an xyz file for analysis
