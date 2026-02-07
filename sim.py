8c91d5833f9d7ffce648d922592d0428a5e7c518
Some_Dumb_Dude <154841872+Tuxedp@users.noreply.github.com>
2026-02-07 16:41:29 -0500

    Add files via upload

---
+++ sim.py
@@ -0,0 +1,14 @@
+# Dependencies (ase, pymatgen)
+from ase.io import read, write
+from ase.visualize import view
+
+# Add a MOF into the simulation through reading the .cif file
+
+# Code to find the center of the MOF's pores in order to add the TFSi anion
+
+# Place Li cations near the Mg sites in the MOF
+
+# Sourround the MOF with  solvent (CH3OCH3)
+
+# Perform an energy minimization to relax the solution
+# Output the relaxed system to an xyz file
